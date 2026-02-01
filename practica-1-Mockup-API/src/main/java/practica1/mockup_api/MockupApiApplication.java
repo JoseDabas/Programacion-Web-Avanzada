855a9e8a52ca82ee419ab42a1eecccfb3f469b8e
Jose Ariel <josearieldabas01@gmail.com>
2026-02-01 18:34:33 -0400

    Estructura inicial del proyecto: Java 21, Boot 3.3.4, Security 6 y JWT

---
+++ practica-1-Mockup-API/src/main/java/practica1/mockup_api/MockupApiApplication.java
@@ -0,0 +1,13 @@
+package practica1.mockup_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockupApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockupApiApplication.class, args);
+	}
+
+}
